ded71e7f58934e8b7792bc515b353830abd4a234
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-13 21:58:39 +0200

    Rendering either all of the country cards or just the results from a search

--- pages/CountryList/index.js
+++ pages/CountryList/index.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Grid } from '@nextui-org/react';
 import CountryCard from '../CountryCard';
 
-const CountryList = ({ countries }) => {
+const CountryList = ({ searchCountries }) => {
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		const getCountries = async () => {
+			const data = await fetch('https://restcountries.com/v2/all');
+			const result = await data.json();
+			setCountries(result);
+		};
+		getCountries();
+	}, []);
+
+	const countrySetToUse = searchCountries.length > 0 ? searchCountries : countries;
+
 	return (
 		<Grid.Container gap={2} justify='center'>
 			{countries &&
-				countries.map((country) => (
+				countrySetToUse.map((country) => (
 					<Grid xs={3}>
 						<CountryCard country={country} key={country.numericCode} />
 					</Grid>
