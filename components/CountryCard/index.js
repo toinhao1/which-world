46e0f44a91ee87b6b14eff5c52949c59a9e6c3eb
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-14 15:23:57 +0200

    Country cards are almost matching light theme design

--- components/CountryCard/index.js
+++ components/CountryCard/index.js
@@ -1,10 +1,10 @@
 import { Card, Image, Link } from '@nextui-org/react';
 
-const CountryCard = ({ country: { flag, name, population, region, capital } }) => {
+const CountryCard = ({ country: { flags, name, population, region, capital } }) => {
 	return (
-		<Link href={`/country-details/${encodeURIComponent(name)}`}>
-			<Card>
-				<Image src={flag} />
+		<Link css={{ color: 'Black' }} href={`/country-details/${encodeURIComponent(name)}`}>
+			<Card css={{ borderRadius: 4, height: 400 }}>
+				<Image height={200} width={284} objectFit='fill' alt={`${name}'s flag`} src={flags.png} />
 				<h4>{name}</h4>
 				<div>Population: {Number(population).toLocaleString()}</div>
 				<div>Region: {region}</div>
--- components/CountryList/index.js
+++ components/CountryList/index.js
@@ -5,7 +5,7 @@ const CountryList = ({ countries, searchCountries }) => {
 	const countrySetToUse = searchCountries.length > 0 ? searchCountries : countries;
 
 	return (
-		<Grid.Container css={{ padding: 0 }} gap={2} justify='center'>
+		<Grid.Container css={{ padding: 0 }} gap={6} justify='center'>
 			{countries &&
 				countrySetToUse.map((country) => (
 					<Grid xs={10} md={3} sm={3} key={country.numericCode}>
