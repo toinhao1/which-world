81f0b70aa80171bad316bc6a25f4ccdf24e2157e
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-14 14:46:54 +0200

    Small styling changes

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import { Row, Text, Container } from '@nextui-org/react';
 const NavBar = () => {
 	return (
 		<Container css={{ boxShadow: '0 2px 4px 0 rgba(0,0,0,.2)', maxWidth: '100%' }}>
-			<Container css={{ padding: 0 }}>
+			<Container>
 				<Row css={{ paddingTop: 20, paddingBottom: 20 }} justify='space-between'>
 					<Text css={{ fontWeight: 800 }}>Which World Is This?</Text>
 					{/* <Text>Toggle theme</Text> */}
