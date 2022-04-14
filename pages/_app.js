a5b0a60d2a31c55fe56b06bbdb57dd5e91c2cb7b
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-14 09:47:28 +0200

    Added the layout component to the app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { NextUIProvider } from '@nextui-org/react';
+import Layout from '../components/layout';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<NextUIProvider>
-			<Component {...pageProps} />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</NextUIProvider>
 	);
 }
