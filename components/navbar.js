cfe93309fdb3384c4cec6b03c6bbe0c97b7451ab
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-14 12:44:23 +0200

    Navbar style seems to be close to design

--- components/navbar.js
+++ components/navbar.js
@@ -2,11 +2,13 @@ import { Row, Text, Container } from '@nextui-org/react';
 
 const NavBar = () => {
 	return (
-		<Container>
-			<Row justify='space-between'>
-				<Text>Which World Is This?</Text>
-				<Text>Toggle theme</Text>
-			</Row>
+		<Container css={{ boxShadow: '0 2px 4px 0 rgba(0,0,0,.2)', maxWidth: '100%' }}>
+			<Container css={{ padding: 0 }}>
+				<Row css={{ paddingTop: 20, paddingBottom: 20 }} justify='space-between'>
+					<Text css={{ fontWeight: 800 }}>Which World Is This?</Text>
+					{/* <Text>Toggle theme</Text> */}
+				</Row>
+			</Container>
 		</Container>
 	);
 };
