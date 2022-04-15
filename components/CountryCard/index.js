62e810661c3bdb6ccadd10296261c22f1412f798
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-15 09:48:23 +0200

    Small styling adjustments

--- components/CountryCard/index.js
+++ components/CountryCard/index.js
@@ -3,7 +3,7 @@ import { Card, Image, Link } from '@nextui-org/react';
 const CountryCard = ({ country: { flags, name, population, region, capital } }) => {
 	return (
 		<Link css={{ color: 'Black' }} href={`/country-details/${encodeURIComponent(name)}`}>
-			<Card css={{ borderRadius: 4, height: 400 }}>
+			<Card css={{ height: 400 }}>
 				<Image height={200} width={284} objectFit='fill' alt={`${name}'s flag`} src={flags.png} />
 				<h4>{name}</h4>
 				<div>Population: {Number(population).toLocaleString()}</div>
