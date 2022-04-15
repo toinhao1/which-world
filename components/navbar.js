31b665a85de40f8c70e91e1ea77ed8635fda864b
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2022-04-15 09:57:47 +0200

    Several css adjustments to match designs closer

--- components/navbar.js
+++ components/navbar.js
@@ -2,7 +2,13 @@ import { Row, Text, Container } from '@nextui-org/react';
 
 const NavBar = () => {
 	return (
-		<Container css={{ boxShadow: '0 2px 4px 0 rgba(0,0,0,.2)', maxWidth: '100%' }}>
+		<Container
+			css={{
+				zIndex: 0,
+				position: 'relative',
+				boxShadow: '0 2px 4px 0 rgba(0,0,0,.2)',
+				maxWidth: '100%',
+			}}>
 			<Container>
 				<Row css={{ paddingTop: 20, paddingBottom: 20 }} justify='space-between'>
 					<Text css={{ fontWeight: 800 }}>Which World Is This?</Text>
